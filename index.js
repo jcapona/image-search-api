4ef6f7dccace2cbfa73df3676cee4663c1172dec
Jorge Capona <jcapona@gmail.com>
2016-01-13 16:01:16 -0300

    Sets routes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var MONGOLAB_URI="mongodb://example:example@ds037415.mongolab.com:37415/url-shortener";
-var DET = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/;
-var base = "https://shortener-basecamp.herokuapp.com/";
 
 var mongo = require('mongodb').MongoClient;
 var express = require('express');
@@ -14,118 +12,55 @@ app.get("/", function(request, response) {
   response.render("index");
 });
 
-app.get("/new/*", function(request, response) {
-  var url = request.params[0];
-  var short_url = "";
-  var json = JSON.stringify({"error":"URL invalid"});
-
-  if(url.match(DET) === null)
-    response.end(json);  
+app.get("/latest", function(request, response) {
   
-  shorten(url,function(err,resp){
+  getLatest(function(err,resp){
     if(err)
+    {
       console.error(err);
-    else
-      json = JSON.stringify({"original_url":url,"short_url":base+resp});
-    
-    response.end(json);
-  });  
-});
-
-app.get("/*", function(request, response) {
-  redir(request.params[0],function(err,resp){
-    if(err)
-      console.error(err);
+      response.end(err);
+    }
     else
     {
       if(resp == null)
-        response.end(JSON.stringify({"error":"No short url found for given input"}));
+        response.end(JSON.stringify({"error":"No history"}));
       else
-        response.redirect(resp);
+        response.end("Displaying latest queries");
+    }
+  });  
+});
+
+app.get("/imagesearch/*", function(request, response) {
+  search(request.params[0],function(err,resp){
+    if(err)
+    {
+      console.error(err);
+      response.end(err);
+    }
+    else
+    {
+      if(resp == null)
+        response.end(JSON.stringify({"error":"No images found for given input"}));
+      else
+        response.end("Displaying images");
     }
   });
 });
 
+app.get("*", function(request, response) {
+  response.redirect("/");
+});
+
+
 app.listen(app.get('port'));
 
-function redir(short_url,callback)
+
+function search(str, callback)
 {
-  mongo.connect(MONGOLAB_URI, function(err, db) {
-    if(err)
-      return callback(err,null);
-
-    db.collection('shorten').find({
-      short: short_url
-    }).toArray(function(err, documents) {
-      if(err) // On error
-      {
-        db.close();
-        callback(err,null);
-      }
-      else if(documents.length) // When url already exists
-      {
-        db.close();
-        var resp = documents[0].url;
-        if(resp.search("://") == -1)
-          resp = "http://"+resp;
-
-
-        callback(null,resp);
-      }
-      else // When it doesnt, returns error
-      {
-        db.close();
-        callback(null,null);
-      }
-    })
-  });
+  callback(str);
 }
 
-
-function shorten(url, callback)
+function getLatest(callback)
 {
-  mongo.connect(MONGOLAB_URI, function(err, db) {
-    if(err)
-      return callback(err,null);
-
-    var col = db.collection('shorten');
-    col.find({
-      url: url
-    }).toArray(function(err, documents) {
-      var resp = "";
-      if(err) // On error
-      {
-        db.close();
-        callback(err,null);
-      }
-      else if(documents.length) // When url already exists
-      {
-        resp = documents[0].short;
-        db.close();
-        callback(null,resp);
-      }
-      else // When it doesnt, create new document in collection
-      {
-        col.count({}, function(err, short_url){
-            if(err) 
-            {
-              db.close();
-              return callback(err);
-            }
-
-            col.insert(({"url":url,"short":short_url.toString()}), function(err, result) {
-              if(err)
-              {
-                db.close();
-                return callback(err);
-              }
-             
-              resp = short_url;
-              db.close();
-              callback(null,resp);
-            });
-        });
-      }
-    })
-  });       
+  callback("Latest");
 }
