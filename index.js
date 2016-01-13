50e66347e20039f7c1107accc8ea49384b72056d
Jorge Capona <jcapona@gmail.com>
2016-01-13 17:51:33 -0300

    Uses GoogleApis to get images

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/latest", function(request, response) {
     }
     else
     {
-      if(resp == null)
+      if(response == null)
         response.end(JSON.stringify({"error":"No history"}));
       else
         response.end("Displaying latest queries");
@@ -31,7 +31,7 @@ app.get("/latest", function(request, response) {
 });
 
 app.get("/imagesearch/*", function(request, response) {
-  search(request.params[0],function(err,resp){
+  searchImgs(request.params[0],function(err,resp){
     if(err)
     {
       console.error(err);
@@ -42,7 +42,7 @@ app.get("/imagesearch/*", function(request, response) {
       if(resp == null)
         response.end(JSON.stringify({"error":"No images found for given input"}));
       else
-        response.end("Displaying images");
+        response.end(JSON.stringify(resp,null,2));
     }
   });
 });
@@ -55,12 +55,56 @@ app.get("*", function(request, response) {
 app.listen(app.get('port'));
 
 
-function search(str, callback)
-{
-  callback(str);
-}
-
 function getLatest(callback)
 {
   callback("Latest");
 }
+
+function searchImgs(str, callback)
+{
+  var google = require('googleapis');
+  var customsearch = google.customsearch('v1');
+
+  const CX = '010473170237434595792:z_8lctunzfu';
+  const API_KEY = 'AIzaSyANNn0S6K_8ytUuCH15l1bROI9S33CpQSk';
+  const SEARCH = str;
+
+  customsearch.cse.list({ cx: CX, q: SEARCH, auth: API_KEY, searchType: "image"}, function(err, resp) 
+  {
+    if (err) {
+      console.log('An error occured', err);
+      callback(err,null);
+    }
+
+    console.log('Result: ' + resp.searchInformation.formattedTotalResults);
+    if (resp.items && resp.items.length > 0) 
+    {
+      makeImgArr(resp.items,function(err,arr){
+        if (err) {
+          console.error(err);
+          callback(err,null);
+        }   
+        else
+          callback(null,arr);
+      });  
+
+    }
+
+  });
+}
+
+function makeImgArr(itemArr, callback)
+{
+  var resArr = [];
+  for(var i=0; i<10; i++)
+  {
+    var obj = {};
+    obj.url = itemArr[i].link;
+    obj.snippet = " "
+    obj.thumbnail = itemArr[i].image.thumbnailLink;
+    obj.context = itemArr[i].image.contextLink;
+    resArr.push(obj);
+  }
+  callback(null,resArr);
+}
+
